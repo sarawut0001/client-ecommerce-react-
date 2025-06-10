cc1555e6cfbbabab035c958b2afc7ae557d32e2b
artzamukie001 <sarawut_boon@elearning.cmu.ac.th>
2025-06-11 00:49:34 +0700

    install vite and create nav bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,8 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 export default Home;
